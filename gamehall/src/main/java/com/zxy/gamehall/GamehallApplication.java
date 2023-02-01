f5563c1b6ec169a10e846ccd0f64cbbf8d075c5b
henrygoal <henry-goal@163.com>
2023-02-01 18:01:59 +0800

    使用本地数据库文件

--- gamehall/src/main/java/com/zxy/gamehall/GamehallApplication.java
+++ gamehall/src/main/java/com/zxy/gamehall/GamehallApplication.java
@@ -1,3 +1,10 @@
+/*
+ * @Description: 
+ * @Author: luyh
+ * @Date: 2023-01-29 16:35:16
+ * @LastEditors: luyh
+ * @LastEditTime: 2023-02-01 18:00:57
+ */
 package com.zxy.gamehall;
 
 import org.springframework.boot.SpringApplication;
@@ -6,8 +13,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GamehallApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GamehallApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GamehallApplication.class, args);
+    }
 
 }
